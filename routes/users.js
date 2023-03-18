005836c684cc6a28677539593fcdf47021ab440a
fishopping marketplace <fishoppingmarket@gmail.com>
2023-03-18 15:13:33 +0330

    complete ui pages and design with bootstrap

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login user listing. */
+router.get('/login', function(req, res, next) {
+  res.render('auth/login',{title:"Login User"});
+});
+
+
+/* GET show register user page. */
+router.get('/create', function(req, res, next) {
+  res.render('auth/register',{title:"Register User"});
+});
+
+/* POST register user listing. */
+router.post('/', function(req, res, next) {
+  let response = req.body;
+  res.json(response);
+  return;
+  const notification = {status:true,message:'example message in here for you'}
+  res.render('auth/register',{title:"Register User",notification:notification});
+});
+
+
+/* GET shwo list of users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('show list of users');
 });
 
 module.exports = router;
